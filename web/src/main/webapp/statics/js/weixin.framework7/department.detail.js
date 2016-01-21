5f68bf41d897143ed17bd1da9284cef857dacdfb
Jiakun Xu <jiakunxu@gmail.com>
2016-01-21 21:17:22 +0800

    demo

--- web/src/main/webapp/statics/js/weixin.framework7/department.detail.js
+++ web/src/main/webapp/statics/js/weixin.framework7/department.detail.js
@@ -19,7 +19,9 @@ myApp.onPageInit('department.detail', function(page) {
 			$$('form.ajax-submit').on('submitted', function(e) {
 						myApp.hideIndicator();
 						var xhr = e.detail.xhr;
-						myApp.alert(xhr.responseText, '信息');
+						myApp.alert(xhr.responseText, '信息', function() {
+									mainView.router.back();
+								});
 					});
 
 			$$('form.ajax-submit').on('submitError', function(e) {
--- web/src/main/webapp/statics/js/weixin.framework7/department.list.js
+++ web/src/main/webapp/statics/js/weixin.framework7/department.list.js
@@ -3,3 +3,31 @@ function department_list_goto(op, id, name) {
 	mainView.router.loadPage(appUrl + "/weixin/department.framework7.htm?op="
 			+ op + "&id=" + id + "&name=" + encodeURIComponent(name));
 }
+
+function department_list_delete(id) {
+	myApp.confirm('确定删除部门？', '部门管理', function() {
+				myApp.showIndicator();
+
+				$$('#department_list_id').val(id);
+				$$('#department/list/delete').trigger("submit");
+			});
+}
+
+myApp.onPageInit('department.list', function(page) {
+			$$('form.ajax-submit').on('beforeSubmit', function(e) {
+					});
+
+			$$('form.ajax-submit').on('submitted', function(e) {
+						myApp.hideIndicator();
+						var xhr = e.detail.xhr;
+						myApp.alert(xhr.responseText, '信息', function() {
+									mainView.router.refreshPage();
+								});
+					});
+
+			$$('form.ajax-submit').on('submitError', function(e) {
+						myApp.hideIndicator();
+						var xhr = e.detail.xhr;
+						myApp.alert(xhr.responseText, '错误');
+					});
+		});
