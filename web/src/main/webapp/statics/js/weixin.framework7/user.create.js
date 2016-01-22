56202808b8a08e224a6aa8a759064cdecf1d334f
Jiakun Xu <jiakunxu@gmail.com>
2016-01-22 18:39:50 +0800

    demo

---
+++ web/src/main/webapp/statics/js/weixin.framework7/user.create.js
@@ -0,0 +1,24 @@
+function user_create() {
+	myApp.showIndicator();
+
+	$$('#user/create').trigger("submit");
+}
+
+myApp.onPageInit('user.create', function(page) {
+			$$('form.ajax-submit').on('beforeSubmit', function(e) {
+					});
+
+			$$('form.ajax-submit').on('submitted', function(e) {
+						myApp.hideIndicator();
+						var xhr = e.detail.xhr;
+						myApp.alert(xhr.responseText, '信息', function() {
+									mainView.router.back();
+								});
+					});
+
+			$$('form.ajax-submit').on('submitError', function(e) {
+						myApp.hideIndicator();
+						var xhr = e.detail.xhr;
+						myApp.alert(xhr.responseText, '错误');
+					});
+		});
