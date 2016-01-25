b1627c56c0ae6e7c58ed48016dc0e928c7379578
Jiakun Xu <jiakunxu@gmail.com>
2016-01-25 17:21:57 +0800

    demo

---
+++ web/src/main/webapp/statics/js/weixin.framework7/message.video.js
@@ -0,0 +1,24 @@
+function message_video() {
+	myApp.showIndicator();
+
+	$$('#message/video').trigger("submit");
+}
+
+myApp.onPageInit('message.video', function(page) {
+			$$('form.ajax-submit').on('beforeSubmit', function(e) {
+					});
+
+			$$('form.ajax-submit').on('submitted', function(e) {
+						myApp.hideIndicator();
+						var xhr = e.detail.xhr;
+						myApp.alert(xhr.responseText, '信息', function() {
+									mainView.router.back();
+								});
+					});
+
+			$$('form.ajax-submit').on('submitError', function(e) {
+						myApp.hideIndicator();
+						var xhr = e.detail.xhr;
+						myApp.alert(xhr.responseText, '错误');
+					});
+		});
