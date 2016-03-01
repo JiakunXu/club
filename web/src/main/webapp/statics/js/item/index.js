82e7e503718fcb7040e33a7994927afd46b4abc3
Jiakun Xu <jiakunxu@gmail.com>
2016-03-01 14:09:54 +0800

    pay

--- web/src/main/webapp/statics/js/item/index.js
+++ web/src/main/webapp/statics/js/item/index.js
@@ -27,12 +27,27 @@ function detail_goto(type) {
 	mainView.router.loadPage(appUrl + "/trade/create.htm?type=" + type);
 }
 
-myApp.onPageInit('ship', function(page) {
-			var e = {
-				provinceId : "province",
-				cityId : "city",
-				countyId : "area",
-				dfCode : '310'
-			};
-			$.fn.cityTools(e);
+myApp.onPageInit('pay.index', function(page) {
+			$$('form.ajax-submit').on('beforeSubmit', function(e) {
+					});
+
+			$$('form.ajax-submit').on('submitted', function(e) {
+						myApp.hideIndicator();
+						var xhr = e.detail.xhr;
+						myApp.alert(xhr.responseText, '信息', function() {
+									mainView.router.back();
+								});
+					});
+
+			$$('form.ajax-submit').on('submitError', function(e) {
+						myApp.hideIndicator();
+						var xhr = e.detail.xhr;
+						myApp.alert(xhr.responseText, '错误');
+					});
 		});
+
+function pay() {
+	myApp.showIndicator();
+
+	$$('#pay/wxpay').trigger("submit");
+}
