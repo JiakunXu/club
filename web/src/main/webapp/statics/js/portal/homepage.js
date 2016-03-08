fbd14973d2903558308c05c2d9e5a0187ce09b04
Jiakun Xu <jiakunxu@gmail.com>
2016-03-08 23:25:15 +0800

    demo

--- web/src/main/webapp/statics/js/portal/homepage.js
+++ web/src/main/webapp/statics/js/portal/homepage.js
@@ -69,51 +69,51 @@ $$('#href-5').on('click', function() {
 		});
 
 myApp.onPageInit('pay.index', function(page) {
-	function pay_index_pay() {
-		myApp.showIndicator();
-
-		$$('#pay/wxpay').trigger("submit");
-	}
-
-	function getBrandWCPayRequest(data) {
-		data = data.replace(/&quot;/g, "\"");
-		var obj = JSON.parse(data);
-
-		try {
-			WeixinJSBridge.invoke('getBrandWCPayRequest', {
-						"appId" : obj.appId,
-						"timeStamp" : obj.timeStamp,
-						"nonceStr" : obj.nonceStr,
-						"package" : obj.packageValue,
-						"signType" : obj.signType,
-						"paySign" : obj.paySign
-					}, function(res) {
-						WeixinJSBridge.log(res.err_msg);
-
-						if (res.err_msg == 'get_brand_wcpay_request:ok') {
-							view2.router.back();
-						} else if (res.err_msg == 'get_brand_wcpay_request:fail') {
-							myApp.alert(res.err_code + res.err_desc
-											+ res.err_msg, '错误');
-						}
+			$$('form.ajax-submit').on('beforeSubmit', function(e) {
 					});
-		} catch (e) {
-			myApp.alert(e, '错误');
-		}
+
+			$$('form.ajax-submit').on('submitted', function(e) {
+						myApp.hideIndicator();
+						var xhr = e.detail.xhr;
+						getBrandWCPayRequest(xhr.responseText);
+					});
+
+			$$('form.ajax-submit').on('submitError', function(e) {
+						myApp.hideIndicator();
+						var xhr = e.detail.xhr;
+						myApp.alert(xhr.responseText, '错误');
+					});
+		});
+
+function pay_index_pay() {
+	myApp.showIndicator();
+
+	$$('#pay/wxpay').trigger("submit");
+}
+
+function getBrandWCPayRequest(data) {
+	data = data.replace(/&quot;/g, "\"");
+	var obj = JSON.parse(data);
+
+	try {
+		WeixinJSBridge.invoke('getBrandWCPayRequest', {
+					"appId" : obj.appId,
+					"timeStamp" : obj.timeStamp,
+					"nonceStr" : obj.nonceStr,
+					"package" : obj.packageValue,
+					"signType" : obj.signType,
+					"paySign" : obj.paySign
+				}, function(res) {
+					WeixinJSBridge.log(res.err_msg);
+
+					if (res.err_msg == 'get_brand_wcpay_request:ok') {
+						view2.router.back();
+					} else if (res.err_msg == 'get_brand_wcpay_request:fail') {
+						myApp.alert(res.err_code + res.err_desc + res.err_msg,
+								'错误');
+					}
+				});
+	} catch (e) {
+		myApp.alert(e, '错误');
 	}
-
-	$$('form.ajax-submit').on('beforeSubmit', function(e) {
-			});
-
-	$$('form.ajax-submit').on('submitted', function(e) {
-				myApp.hideIndicator();
-				var xhr = e.detail.xhr;
-				getBrandWCPayRequest(xhr.responseText);
-			});
-
-	$$('form.ajax-submit').on('submitError', function(e) {
-				myApp.hideIndicator();
-				var xhr = e.detail.xhr;
-				myApp.alert(xhr.responseText, '错误');
-			});
-});
+}
